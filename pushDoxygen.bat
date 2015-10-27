e7da4b0cfaeb28b1687526a2a79ca2994b6594bb
Geroshabu <oonstks@gmail.com>
2015-10-27 22:41:06 +0900

    refs #10 Add a bat file to push Doxygen products :octocat:

---
+++ pushDoxygen.bat
@@ -0,0 +1,13 @@
+git clone https://github.com/Geroshabu/Geroshabu.github.io.git C:\projects\gh-pages
+cd C:/projects/gh-pages
+if exist html (rd html /s /q)
+doxygen C:/projects/GeroMachine/Doxyfile
+if exist latex (rd latex /s /q)
+cd html
+git status | findstr /C:"Changes not staged for commit:" /C:"Untracked files:"
+if Not ERRORLEVEL 1 (
+    git add --all .
+    git commit -m "auto commit doxygen products by AppVeyor" -m %APPVEYOR_REPO_COMMIT%
+    git push
+)
+
