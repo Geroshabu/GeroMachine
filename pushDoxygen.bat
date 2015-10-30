baf6265a5f312b5e0031040efc20df01f4c555a3
Geroshabu <oonstks@gmail.com>
2015-10-30 20:49:10 +0900

    refs #15 Allow to push doxygen documents only master branch :octocat:

--- pushDoxygen.bat
+++ pushDoxygen.bat
@@ -2,13 +2,15 @@ if "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
     git config --global user.email %MailAddress%
     git config --global user.name %UserName%
     git config --global push.default matching
-    git clone --quiet --branch=gh-pages https://github.com/Geroshabu/GeroMachine.git C:\projects\gh-pages
-    cd C:/projects/gh-pages
-    if exist html (rd html /s /q)
-    doxygen C:/projects/GeroMachine/Doxyfile
-    if exist latex (rd latex /s /q)
-    cd html
-    git add --all .
-    git commit -m "auto commit doxygen products by AppVeyor" -m %APPVEYOR_REPO_COMMIT%
-    git push --quiet https://%GitHubAccessToken%@github.com/Geroshabu/GeroMachine gh-pages
+    if "%APPVEYOR_REPO_BRANCH%"=="master" (
+        git clone --quiet --branch=gh-pages https://github.com/Geroshabu/GeroMachine.git C:\projects\gh-pages
+        cd C:/projects/gh-pages
+        if exist html (rd html /s /q)
+        doxygen C:/projects/GeroMachine/Doxyfile
+        if exist latex (rd latex /s /q)
+        cd html
+        git add --all .
+        git commit -m "auto commit doxygen products by AppVeyor" -m %APPVEYOR_REPO_COMMIT%
+        git push --quiet https://%GitHubAccessToken%@github.com/Geroshabu/GeroMachine gh-pages
+    )
 )
