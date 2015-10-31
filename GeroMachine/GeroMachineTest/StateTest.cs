9d9c8c575abd9355fbaa74d5fe11dfc9517db4b6
Geroshabu <oonstks@gmail.com>
2015-11-01 01:39:35 +0900

    refs #32 Add a test case for CheckTrigger method :white_check_mark:

--- GeroMachine/GeroMachineTest/StateTest.cs
+++ GeroMachine/GeroMachineTest/StateTest.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace GeroMachineTest
 {
 	public class StateTest
 	{
+		[Fact]
+		public void PassingTest()
+		{
+			GeroMachine.State state = new GeroMachine.State();
+			Assert.Equal(0, state.CheckTrigger());
+		}
 	}
 }
