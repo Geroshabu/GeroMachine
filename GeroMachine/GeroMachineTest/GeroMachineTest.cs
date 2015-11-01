d0c8a40b7323b07dd96072614d36443fc8e30651
Geroshabu <oonstks@gmail.com>
2015-11-01 19:59:49 +0900

    refs #38 Add a test class for construction tests :white_check_mark:

--- GeroMachine/GeroMachineTest/GeroMachineTest.cs
+++ GeroMachine/GeroMachineTest/GeroMachineTest.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace GeroMachineTest
 {
-	class GeroMachineTest
+	public class GeroMachineTest
 	{
+		/// <summary>
+		/// StateMachineを作成する前のテスト
+		/// </summary>
+		public class BeforeCreationTest
+		{
+		}
 	}
 }
