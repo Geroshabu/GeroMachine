6ace75739edc3baa83d6e497a891cac889fd634a
Geroshabu <oonstks@gmail.com>
2015-11-02 13:03:05 +0900

    refs #39 Declare member signatures :footprints:

--- GeroMachine/GeroMachine/Trigger.cs
+++ GeroMachine/GeroMachine/Trigger.cs
@@ -2,5 +2,32 @@ namespace GeroMachine
 {
 	public class Trigger
 	{
+		/// <summary>
+		/// トリガが発生したかどうかを取得する.
+		/// <para>
+		/// このプロパティによりトリガの発生状況を取得した後は,
+		/// このプロパティ値は<c>false</c>になる.
+		/// </para>
+		/// </summary>
+		/// <value>トリガが発生していれば<c>true</c>, 発生していなければ<c>false</c></value>
+		public bool HasOccured { get; protected set; }
+
+		/// <summary>
+		/// このトリガのID
+		/// </summary>
+		/// <remarks>これはユーザに関係ない情報. 後で消す.</remarks>
+		public uint Id { get; protected set; }
+
+		/// <summary>
+		/// <seealso cref="Id"/>を採番するための値.
+		/// </summary>
+		private static uint CountOfTrigger = 0;
+
+		/// <summary>
+		/// <see cref="Trigger"/>クラスの新しいインスタンスを初期化する.
+		/// </summary>
+		public Trigger()
+		{
+		}
 	}
 }
