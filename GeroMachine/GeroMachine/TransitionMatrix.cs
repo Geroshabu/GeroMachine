93c3bb970c95c5103e650df76121b8af3f0d08c1
Geroshabu <oonstks@gmail.com>
2015-11-25 22:46:39 +0900

    refs #43 Define interfaces to control the transition matrix

--- GeroMachine/GeroMachine/TransitionMatrix.cs
+++ GeroMachine/GeroMachine/TransitionMatrix.cs
@@ -1,6 +1,41 @@
+using System;
+using System.Collections.Generic;
+
 namespace GeroMachine
 {
-	class TransitionMatrix
+	/// <summary>
+	/// 状態遷移表を管理する機能を提供する
+	/// </summary>
+	public class TransitionMatrix : ITransitionMatrix
 	{
+		/// <summary>
+		/// 遷移表の本体
+		/// </summary>
+		private Dictionary<State, Dictionary<Trigger, Transition>> MatrixData;
+
+		/// <summary>
+		/// <see cref="TransitionMatrix"/>クラスの新しいインスタンスを初期化する.
+		/// </summary>
+		/// <param name="matrix">遷移表のもとになる2次元ハッシュテーブル</param>
+		/// <exception cref="ArgumentNullException">引数<paramref name="transitionMatrix"/>がnull</exception>
+		public TransitionMatrix(Dictionary<State, Dictionary<Trigger, Transition>> transitionMatrix)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// 状態が<paramref name="currentState"/>のときにトリガ<paramref name="trigger"/>が発生した場合の,
+		/// 起こる遷移を取得する.
+		/// </summary>
+		/// <param name="currentState">現在の状態</param>
+		/// <param name="trigger">発生したトリガ</param>
+		/// <returns><paramref name="currentState"/>で<paramref name="trigger"/>が発生したときに起こる遷移.
+		/// 起こる遷移が無ければ, nullが返る.</returns>
+		/// <exception cref="ArgumentNullException">引数<paramref name="currentState"/>がnull,
+		/// または引数<paramref name="trigger"/>がnull.</exception>
+		public Transition SearchTransition(State currentState, Trigger trigger)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
