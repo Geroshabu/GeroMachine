8070393a685a5a6a43c9fe431e98ffd04e1007d3
Geroshabu <oonstks@gmail.com>
2015-12-02 23:27:46 +0900

    refs #47 Add buttons on a form to control a statemachine :arrow_forward:

--- GeroMachine/GeroMachineSample/GeroMachineSampleForm.Designer.cs
+++ GeroMachine/GeroMachineSample/GeroMachineSampleForm.Designer.cs
@@ -28,40 +28,73 @@ namespace GeroMachineSample
         /// </summary>
         private void InitializeComponent()
         {
-            this.RunButton = new System.Windows.Forms.Button();
-            this.CurrentStateIdLabel = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // RunButton
-            // 
-            this.RunButton.Location = new System.Drawing.Point(108, 112);
-            this.RunButton.Name = "RunButton";
-            this.RunButton.Size = new System.Drawing.Size(75, 23);
-            this.RunButton.TabIndex = 0;
-            this.RunButton.Text = "Run";
-            this.RunButton.UseVisualStyleBackColor = true;
-            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
-            // 
-            // CurrentStateIdLabel
-            // 
-            this.CurrentStateIdLabel.AutoSize = true;
-            this.CurrentStateIdLabel.Location = new System.Drawing.Point(124, 175);
-            this.CurrentStateIdLabel.Name = "CurrentStateIdLabel";
-            this.CurrentStateIdLabel.Size = new System.Drawing.Size(31, 12);
-            this.CurrentStateIdLabel.TabIndex = 1;
-            this.CurrentStateIdLabel.Text = "None";
-            // 
-            // GeroMachineSampleForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.CurrentStateIdLabel);
-            this.Controls.Add(this.RunButton);
-            this.Name = "GeroMachineSampleForm";
-            this.Text = "GeroMachineSampleForm";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.RunButton = new System.Windows.Forms.Button();
+			this.CurrentStateIdLabel = new System.Windows.Forms.Label();
+			this.Trigger1Button = new System.Windows.Forms.Button();
+			this.Trigger2Button = new System.Windows.Forms.Button();
+			this.Trigger3Button = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// RunButton
+			// 
+			this.RunButton.Location = new System.Drawing.Point(93, 137);
+			this.RunButton.Name = "RunButton";
+			this.RunButton.Size = new System.Drawing.Size(75, 23);
+			this.RunButton.TabIndex = 0;
+			this.RunButton.Text = "Run";
+			this.RunButton.UseVisualStyleBackColor = true;
+			this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+			// 
+			// CurrentStateIdLabel
+			// 
+			this.CurrentStateIdLabel.AutoSize = true;
+			this.CurrentStateIdLabel.Location = new System.Drawing.Point(116, 179);
+			this.CurrentStateIdLabel.Name = "CurrentStateIdLabel";
+			this.CurrentStateIdLabel.Size = new System.Drawing.Size(31, 12);
+			this.CurrentStateIdLabel.TabIndex = 1;
+			this.CurrentStateIdLabel.Text = "None";
+			// 
+			// Trigger1Button
+			// 
+			this.Trigger1Button.Location = new System.Drawing.Point(12, 69);
+			this.Trigger1Button.Name = "Trigger1Button";
+			this.Trigger1Button.Size = new System.Drawing.Size(75, 23);
+			this.Trigger1Button.TabIndex = 2;
+			this.Trigger1Button.Text = "Trigger1";
+			this.Trigger1Button.UseVisualStyleBackColor = true;
+			// 
+			// Trigger2Button
+			// 
+			this.Trigger2Button.Location = new System.Drawing.Point(93, 69);
+			this.Trigger2Button.Name = "Trigger2Button";
+			this.Trigger2Button.Size = new System.Drawing.Size(75, 23);
+			this.Trigger2Button.TabIndex = 3;
+			this.Trigger2Button.Text = "Trigger2";
+			this.Trigger2Button.UseVisualStyleBackColor = true;
+			// 
+			// Trigger3Button
+			// 
+			this.Trigger3Button.Location = new System.Drawing.Point(174, 69);
+			this.Trigger3Button.Name = "Trigger3Button";
+			this.Trigger3Button.Size = new System.Drawing.Size(75, 23);
+			this.Trigger3Button.TabIndex = 4;
+			this.Trigger3Button.Text = "Trigger3";
+			this.Trigger3Button.UseVisualStyleBackColor = true;
+			// 
+			// GeroMachineSampleForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(259, 218);
+			this.Controls.Add(this.Trigger3Button);
+			this.Controls.Add(this.Trigger2Button);
+			this.Controls.Add(this.Trigger1Button);
+			this.Controls.Add(this.CurrentStateIdLabel);
+			this.Controls.Add(this.RunButton);
+			this.Name = "GeroMachineSampleForm";
+			this.Text = "GeroMachineSampleForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -69,6 +102,9 @@ namespace GeroMachineSample
 
         private System.Windows.Forms.Button RunButton;
         private System.Windows.Forms.Label CurrentStateIdLabel;
-    }
+		private System.Windows.Forms.Button Trigger1Button;
+		private System.Windows.Forms.Button Trigger2Button;
+		private System.Windows.Forms.Button Trigger3Button;
+	}
 }
 
