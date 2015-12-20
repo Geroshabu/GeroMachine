27a3c0912aa2d9a463f4ba5923a0a7361d415413
Geroshabu <oonstks@gmail.com>
2015-12-20 13:14:30 +0900

    refs #64 Allow discrimination of State types :footprints:

--- GeroMachine/GeroMachine/State.cs
+++ GeroMachine/GeroMachine/State.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace GeroMachine
 {
+	/// <summary>
+	///	状態の種類
+	/// </summary>
+	public enum StateType
+	{
+		NormalState,
+		EndState
+	}
+
 	public abstract class State
 	{
 		/// <summary>
 		/// この状態を識別するための情報 (デバッグ用)
 		/// </summary>
 		public string StateName { get; set; }
+
+		/// <summary>
+		/// この状態の種類
+		/// </summary>
+		public StateType StateType { get; private set; }
 
 		public State()
 		{
