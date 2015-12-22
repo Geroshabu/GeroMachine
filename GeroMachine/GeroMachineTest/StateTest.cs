63a09c99ea8b432cc9daf8946eed48773db2212f
Geroshabu <oonstks@gmail.com>
2015-12-22 23:26:42 +0900

    refs #64 Prepare a class derived from the State class for tests :white_check_mark:

--- GeroMachine/GeroMachineTest/StateTest.cs
+++ GeroMachine/GeroMachineTest/StateTest.cs
@@ -1,6 +1,15 @@
+using GeroMachine;
+
 namespace GeroMachineTest
 {
 	public class StateTest
 	{
+		public class CreationTest
+		{
+			private class DerivedState : State
+			{
+				public DerivedState(StateType stateType) : base(stateType) { }
+			}
+		}
 	}
 }
