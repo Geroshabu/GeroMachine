105057cc6d75357228f6d3f9c54f18548150be8b
Geroshabu <oonstks@gmail.com>
2015-12-23 00:18:10 +0900

    refs #64 Allow test methods to work :white_check_mark:

--- GeroMachine/GeroMachineTest/StateTest.cs
+++ GeroMachine/GeroMachineTest/StateTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using Xunit;
 using GeroMachine;
 
@@ -15,11 +17,25 @@ namespace GeroMachineTest
 			[Fact(DisplayName = "State:Creation:コンストラクタの単純な値初期化テスト", Skip = "Not Implemented")]
 			public void TestConstructor()
 			{
+				// Execute
+				DerivedState instance = new DerivedState(StateType.NormalState);
+
+				// Get Result
+				FieldInfo field_info = instance.GetType().BaseType.GetField("<StateType>k__BackingField",
+					BindingFlags.GetField | BindingFlags.NonPublic | BindingFlags.Instance);
+				StateType actual_StateType = (StateType)field_info.GetValue(instance);
+
+				// Validate
+				Assert.Equal(StateType.NormalState, actual_StateType);
 			}
 
 			[Fact(DisplayName = "State:Creation:コンストラクタの引数に不正な値が渡されるテスト", Skip = "Not Implemented")]
 			public void TestOutOfRangeArgumentConstructor()
 			{
+				// Execute & Validate
+				DerivedState instance;
+				Assert.Throws<ArgumentOutOfRangeException>("stateType",
+					() => instance = new DerivedState((StateType)2));
 			}
 		}
 	}
