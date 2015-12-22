fed0936cc829a3ac7f6423a063f45cee2eaa730f
Geroshabu <oonstks@gmail.com>
2015-12-23 00:12:58 +0900

    refs #64 Define test cases :white_check_mark:

--- GeroMachine/GeroMachineTest/StateTest.cs
+++ GeroMachine/GeroMachineTest/StateTest.cs
@@ -1,3 +1,4 @@
+using Xunit;
 using GeroMachine;
 
 namespace GeroMachineTest
@@ -9,6 +10,16 @@ namespace GeroMachineTest
 			private class DerivedState : State
 			{
 				public DerivedState(StateType stateType) : base(stateType) { }
+			}
+
+			[Fact(DisplayName = "State:Creation:コンストラクタの単純な値初期化テスト", Skip = "Not Implemented")]
+			public void TestConstructor()
+			{
+			}
+
+			[Fact(DisplayName = "State:Creation:コンストラクタの引数に不正な値が渡されるテスト", Skip = "Not Implemented")]
+			public void TestOutOfRangeArgumentConstructor()
+			{
 			}
 		}
 	}
